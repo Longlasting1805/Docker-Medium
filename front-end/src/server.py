88dd0efbc420d9a0da68b69942157e6f3cd4aa6f
Akande Al-ameen <longlasting1805@gmail.com>
2022-10-10 19:25:32 +0100

    ping ip

--- front-end/src/server.py
+++ front-end/src/server.py
@@ -1,12 +1,12 @@
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
-# app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 @app.route('/')
 def show_books():
     # Your Container IP instead
-    Books = requests.get('http://172.18.0.2:5000/books').json()  
+    Books = requests.get('http://172.18.0.3:5000/books').json()  
     return render_template('show_books.html', books=Books)
 
 if __name__ == '__main__':
