a8bea8187efd3f62250a8d7fcae8b12ea8fb825c
Akande Al-ameen <longlasting1805@gmail.com>
2022-10-12 18:42:46 +0100

    imoirting json

--- front-end/src/server.py
+++ front-end/src/server.py
@@ -1,12 +1,15 @@
+import requests
 from flask import Flask, render_template
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def show_books():
     # Your Container IP instead
-    Books = requests.get('http://book-api:5000/books').json()  
-    return render_template('show_books.html', books=Books)
+    Books = requests.get('http://172.18.0.2:5000/books')
+    return render_template('show_books.html', books=Books).json()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
